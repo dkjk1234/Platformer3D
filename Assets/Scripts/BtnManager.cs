32a94c08bfef61bfe9f2ca9de082a5b548cf6074
Junyuhan04 <junyuhan04@gmail.com>
2023-06-12 23:21:46 +0900

    Loading Scene

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnManager : MonoBehaviour
+{
+    int SceneNum;
+
+    public void Title_Start()
+    {
+        SceneNum = 1;
+        StartCoroutine(Scene_Move());
+
+    }
+    public void Title_HowtoPlay()
+    {
+        SceneNum = 2;
+        StartCoroutine(Scene_Move());
+    }
+
+    public void Title_Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif 
+        Application.Quit();
+    }
+
+    IEnumerator Scene_Move()
+    {
+        yield return new WaitForSeconds(0.5f);
+        switch (SceneNum)
+        {
+            case 1:
+                Debug.Log("인게임 입장");
+                LoadingScene.LoadScene("InGame");
+                break;
+            case 2:
+                Debug.Log("게임 방법 씬 입장");
+                LoadingScene.LoadScene("HowtoPlay");
+                break;
+            case 3:
+                Debug.Log("게임방법에서 메뉴로 이동");
+                LoadingScene.LoadScene("Title");
+                break;
+        }
+    }
+}
