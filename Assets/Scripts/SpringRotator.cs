cebe8f4eea00621b84e2512b167bc9c0c1ba51da
DakRo <dkjk1234@naver.com>
2023-06-13 00:33:16 +0900

    톱니 통나무 프리팹, 스크립트 추가

---
+++ Assets/Scripts/SpringRotator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringRotator : MonoBehaviour
+{
+    public float rotationSpeed = 50f; // 회전 속도를 설정합니다. 원하는 속도로 조절 가능합니다.
+    public int rotateXYZNumState = 0;
+    public bool isReverse = false;
+
+    void Update()
+    {
+        if(rotateXYZNumState == 0)
+        transform.Rotate( rotationSpeed * Time.deltaTime * (isReverse ? 1 : -1),0,1, 0); // Y 축을 중심으로 회전합니다.
+        if(rotateXYZNumState == 1)
+            transform.Rotate( 0,rotationSpeed * Time.deltaTime * (isReverse ? 1 : -1), 0); // Y 축을 중심으로 회전합니다.
+        if(rotateXYZNumState == 2)
+            transform.Rotate( 0,0, rotationSpeed * Time.deltaTime * (isReverse ? 1 : -1)); // Y 축을 중심으로 회전합니다.
+    }
+}
+
